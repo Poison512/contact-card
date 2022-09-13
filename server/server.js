6e271f274deaed9211a72b4ce367c69d0db31e24
Poison512 <adrian512@Adrians-MBP.attlocal.net>
2022-09-13 07:48:40 -0500

    Add to staging Lesson 2

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3002;
 app.use(express.static('../client/'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+`app.use(express.static(’../client/dist/’))`
 
 require('./routes/htmlRoutes.js')(app);
 
